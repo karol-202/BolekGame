8f27e88404c99a7d7017c620e65bdc2848e56284
karol-202 <karoljurski1@gmail.com>
2018-03-14 23:22:37 +0100

    Add user volume management.

--- app/src/main/java/pl/karol202/bolekgame/game/players/NonPlayersAdapter.java
+++ app/src/main/java/pl/karol202/bolekgame/game/players/NonPlayersAdapter.java
@@ -1,13 +1,20 @@
 package pl.karol202.bolekgame.game.players;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import java8.util.stream.Collectors;
 import pl.karol202.bolekgame.R;
+import pl.karol202.bolekgame.server.RemoteUser;
 import pl.karol202.bolekgame.server.User;
 
 import java.util.ArrayList;
@@ -18,16 +25,68 @@ class NonPlayersAdapter extends RecyclerView.Adapter<NonPlayersAdapter.ViewHolde
 	class ViewHolder extends RecyclerView.ViewHolder
 	{
 		private TextView textName;
+		private ImageButton buttonSettings;
+		
+		private User user;
 		
 		ViewHolder(View view)
 		{
 			super(view);
 			textName = view.findViewById(R.id.text_non_player_name);
+			buttonSettings = view.findViewById(R.id.button_user_settings);
+			buttonSettings.setOnClickListener(v -> showSettingsWindow());
 		}
 		
 		void bind(User user)
 		{
+			this.user = user;
+			
 			textName.setText(user.getName());
+			buttonSettings.setEnabled(user instanceof RemoteUser);
+		}
+		
+		@SuppressLint("InflateParams")
+		private void showSettingsWindow()
+		{
+			if(user == null || !(user instanceof RemoteUser)) return;
+			RemoteUser user = (RemoteUser) this.user;
+			PopupWindow window = new PopupWindow(context);
+			
+			LayoutInflater inflater = LayoutInflater.from(context);
+			View view = inflater.inflate(R.layout.popup_user_settings, null);
+			onSettingsViewCreate(view, user);
+			
+			window.setContentView(view);
+			window.showAsDropDown(buttonSettings);
+		}
+		
+		private void onSettingsViewCreate(View view, RemoteUser user)
+		{
+			initMutePanel(view, user);
+		}
+		
+		private void initMutePanel(View view, RemoteUser user)
+		{
+			View panelMute = view.findViewById(R.id.panel_user_mute);
+			panelMute.setOnClickListener(v -> toggleUserMute());
+			
+			ImageView imageMute = panelMute.findViewById(R.id.image_user_mute);
+			imageMute.setImageResource(user.isMuted() ? R.drawable.ic_speaker_on_black_24dp : R.drawable.ic_speaker_off_black_24dp);
+			
+			TextView textMute = panelMute.findViewById(R.id.text_user_mute);
+			textMute.setText(user.isMuted() ? R.string.text_user_unmute : R.string.text_user_mute);
+		}
+		
+		private void toggleUserMute(RemoteUser user)
+		{
+			user.setMute(!user.isMuted());
+		}
+		
+		private void initVolumePanel(View view, RemoteUser user)
+		{
+			View panelVolume = view.findViewById(R.id.panel_user_volume);
+			
+			SeekBar seekBarVolume = panelVolume.findViewById(R.id.seekBar_user_volume);
 		}
 	}
 	
--- app/src/main/java/pl/karol202/bolekgame/server/RemoteUser.java
+++ app/src/main/java/pl/karol202/bolekgame/server/RemoteUser.java
@@ -7,10 +7,16 @@ public class RemoteUser extends User
 {
 	private InetAddress address;
 	
+	private float volume;
+	private boolean mute;
+	
 	RemoteUser(String name, boolean ready, String address)
 	{
 		super(name, ready);
 		createInetAddress(address);
+		
+		volume = 1;
+		mute = false;
 	}
 	
 	private void createInetAddress(String address)
@@ -28,5 +34,26 @@ public class RemoteUser extends User
 	public InetAddress getAddress()
 	{
 		return address;
+	}
+
+	public float getVolume()
+	{
+		return volume;
+	}
+	
+	public void setVolume(float volume)
+	{
+		if(volume < 0 || volume > 1) return;
+		this.volume = volume;
+	}
+	
+	public boolean isMuted()
+	{
+		return mute;
+	}
+	
+	public void setMute(boolean mute)
+	{
+		this.mute = mute;
 	}
 }
